b23a989280d7b4873c1f2fdcbffee7da8875a9ab
himu <satirtharoy2003@gmail.com>
2024-02-03 19:38:14 +0600

    conditional rendering in main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
 export default Main
 
+const Hellow = (props) => <h3>hellow {props.name}</h3>;
+const NotHellow = (props) => <h3>Get out</h3>;
+
+function Msg (props) {
+  return props.name === 'himu' ? <Hellow name={props.name}/> : <NotHellow/>;
+}
+
 function Main (props) {
+  let name ='onno';
   return <main>
     <h2>Main</h2>
-    {props.bag}
+    <Msg name={name}/>
   </main>
 }
