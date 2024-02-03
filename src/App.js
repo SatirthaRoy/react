1f9f7108c20ee017b1151835cacfc6e41bc03840
himu <satirtharoy2003@gmail.com>
2024-02-03 15:56:08 +0600

    nothing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Navigation from "./components/Navigation";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import Bag from "./components/Bag";
-import Fruits from "./components/Fruits";
 
 function App() {
   return (
     <>
       <Navigation firstName = 'Satirtha' lastName="Roy"/>
-      <Main bag={<Bag fruits={<Fruits/>}/>}/>
+      <Main/>
       <Footer list={['rice', 'potato', 'sugar', 'mango', 'apple', 'fish']}/>
     </>
   );
