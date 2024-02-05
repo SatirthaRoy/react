f886ace580e1ebaef7270c6cfaa0a7b0608dc335
himu <satirtharoy2003@gmail.com>
2024-02-05 17:25:46 +0600

    react router uses

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import { createContext, useState } from "react";
-import Todo from "./components/Todo";
+import About from "./components/About";
+import Homepage from "./components/Homepage";
+import {Route, Routes, Link} from 'react-router-dom';
 
 export const Log = createContext();
 
 function App() {
-  const credentials = {
-    email: 'a',
-    pass: 'a'
-  }
-  const [isLoggedIn, setIsLoggedIn] = useState(false);  
-
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
-
-
-  function Logged () {
-    return isLoggedIn ? <Log.Provider value = {isLoggedIn}>
-      <Todo/>
-    </Log.Provider> : <h3>wrong email or password</h3>
-  }
-
-  let [showLog, setShowLog] = useState(false);
 
   return (
     <>
-    <h1>hellow</h1>
-    <p>email: 'a' pass 'a'</p>
-    <div>email: <input type="email" onChange={(e) => {
-        setEmail(e.target.value);
-      }} value={email}/>
-    </div>
-    <div>password: <input type="password" onChange={(e) => {
-        setPass(e.target.value);
-      }} value={pass}/>
-    </div>
-    {!isLoggedIn && <button onClick={() => {
-      const info = {email: email, pass: pass};
-      console.log(`info: ${info.email} ${info.pass}`);
-      setIsLoggedIn((info.email === credentials.email && info.pass === credentials.pass));
-      setEmail('');
-      setPass('');
-      setShowLog(true);
-    }}>Log in</button>}
-    
-      {showLog && <Logged/>}
+      <ul>
+        <li><Link to='/'>Homepage</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+
     </>
   );
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,6 @@
+
+function Homepage() {
+  return <h2>Welcome to homepage</h2>;
+}
+
+export default Homepage;
