ab066eff9834d37c4eafed84fdfe937e5504c785
himu <satirtharoy2003@gmail.com>
2024-02-07 17:36:36 +0600

    calculator using react and simple-codes package

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { createContext, useState } from "react";
 import About from "./components/About";
 import Homepage from "./components/Homepage";
+import Calculator from "./components/Calculator";
 import {Route, Routes, Link} from 'react-router-dom';
 
 export const Log = createContext();
 
 function App() {
-
   return (
     <>
       <ul>
         <li><Link to='/'>Homepage</Link></li>
         <li><Link to='/about'>About</Link></li>
+        <li><Link to='/calc'>Calculator</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/calc" element={<Calculator/>}/>
       </Routes>
 
     </>
