02e79508dc6c070362677bb5820e549599723b0a
himu <satirtharoy2003@gmail.com>
2024-02-09 00:56:46 +0600

    todo list with react js soo good

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext} from "react";
 import About from "./components/About";
 import Homepage from "./components/Homepage";
 import Calculator from "./components/Calculator";
+import Todo from "./components/Todo";
 import {Route, Routes, Link} from 'react-router-dom';
 
 export const Log = createContext();
@@ -13,11 +14,13 @@ function App() {
         <li><Link to='/'>Homepage</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/calc'>Calculator</Link></li>
+        <li><Link to='/todo'>Todo</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/calc" element={<Calculator/>}/>
+        <Route path="/todo" element={<Todo/>}/>
       </Routes>
 
     </>
