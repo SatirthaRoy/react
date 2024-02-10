58fbfe67a514408b37474616e048cb224fe2d232
himu <satirtharoy2003@gmail.com>
2024-02-10 18:55:04 +0600

    added a controlled form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Homepage from "./components/Homepage";
 import Calculator from "./components/Calculator";
 import Todo from "./components/Todo";
+import Form from "./components/Form";
 import {Route, Routes, Link} from 'react-router-dom';
 
 export const Log = createContext();
@@ -15,12 +16,14 @@ function App() {
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/calc'>Calculator</Link></li>
         <li><Link to='/todo'>Todo</Link></li>
+        <li><Link to='/form'>Form</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/calc" element={<Calculator/>}/>
         <Route path="/todo" element={<Todo/>}/>
+        <Route path="/form" element={<Form/>}/>
       </Routes>
 
     </>
---
+++ src/components/Form.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Form () {
+  const [form,setForm] = useState({name: '', email: '', feedback: ''});
+  let disable = ((form.name !== '') && (form.email !== '') && (form.feedback !== '')) ? false : true;
+
+  const [info, setInfo] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInfo({...form});
+    setForm({...form, name: '', email: '', feedback: ''});
+  }
+  return <form action="" onSubmit={handleSubmit}>
+    <fieldset>
+      <h2>Feedback form</h2>
+      <label htmlFor="name">
+        Name: <input type="text" placeholder="Enter name" value={form.name} onChange={e => setForm({...form, name: e.target.value})}/>
+      </label>
+      <br />
+      <label htmlFor="email">
+        Email: <input type="email" placeholder="Enter email" value={form.email} onChange={e => setForm({...form, email: e.target.value})}/>
+      </label>
+      <br />
+      <label htmlFor="feedback">Feedback:</label>
+      <br />
+      <textarea name="feedback" id="feedback" placeholder="Enter feedback" cols="30" rows="10" value={form.feedback} onChange={e => setForm({...form, feedback: e.target.value})}></textarea>
+      <br />
+      <input type="submit" value="Submit" disabled={disable}/>
+      <h4>Name: {info.name}</h4>
+      <h4>Email: {info.email}</h4>
+      <h4>Feedback: {info.feedback}</h4>
+    </fieldset>
+  </form>
+}
+
+export default Form;
