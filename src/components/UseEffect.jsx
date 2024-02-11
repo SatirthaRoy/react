86eb1ef782c8c3e824ffc3b4a3ddfd5c4e40de9d
himu <satirtharoy2003@gmail.com>
2024-02-11 22:12:28 +0600

    fetching from a link in useEffect

--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -3,20 +3,47 @@ import { useEffect, useState } from "react";
 
 
 function UseEffect () {
-  let [count,setCount] = useState(0);
-  let [toogle,setToogle] = useState(false);
-  const fuckCount = () => {
-    setCount(count+=1);
+  let [users,setUsers] = useState([]);
+  const fetchData = () => {
+    fetch('https://randomuser.me/api/?results=50')
+    .then(Response => Response.json())
+    .then(data => {
+      setUsers(data.results);
+      console.log(data.results);
+    })
+    .catch(e => console.log(e));
   }
-  useEffect(()=>{
-     document.title = toogle ? "fuck you" : "fuck you more";
-     console.log(count);
-    }, [toogle, count]);
-    
+
+  useEffect(()=>fetchData(),[]);
+
+  function ShowResult(){
+    if(users.length > 0) {
+      return <h1>Received</h1>
+    } else {
+      return <h1>fetching....</h1>
+    }
+  }
+
+  function Cards() {
+    return <div className="container">
+      {users.map(user => {
+        return <div className="card" key={user.cell}>
+          <div className="pic">
+            <img src={user.picture.large} alt="" />
+          </div>
+          <h3>{Object.values(user.name).join(' ')}</h3>
+          <p>Address: {user.location.city},{user.location.country}</p>
+          <p>Cell phone: {user.phone}</p>
+          <p>Gender: {user.gender}</p>
+          <label htmlFor="email">Email: <input type="email" value={user.email} readOnly/></label>
+        </div>
+      })}
+    </div>
+  }
+
   return <div>
-    {count}
-    <button onClick={fuckCount}>+</button>
-    <button onClick={()=> setToogle(!toogle)}>Fuck toogle</button>
+    <ShowResult/>
+    <Cards/>
   </div>
 }
 
